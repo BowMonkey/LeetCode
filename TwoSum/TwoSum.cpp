96d36b395abb2d7271e9141dc478e5e747921a11
BowMonkey <1457936638@qq.com>
2017-03-19 10:31:18 +0800

    TwoSum Accepted! Yes!

--- TwoSum/TwoSum.cpp
+++ TwoSum/TwoSum.cpp
@@ -1,14 +1,93 @@
 
-
 #include<stdio.h>
-int twoSum(int *nums, int numsSize, int target);
+#include<stdlib.h>
+
+int *twoSum(int *nums, int numsSize, int target);
+void sort(int *array, int size);
+
+
 int main(void)
 {
-
+	int *p;
+	int target = 200;
+	int nums[] = { 150,24,79,50,88,345,3};
+	p = twoSum(nums, sizeof(nums) / sizeof(nums[0]), target);
 	return 0;
 }
 
-int twoSum(int *nums, int numsSize, int target)
+int *twoSum(int *nums, int numsSize, int target)
 {
+	int lo, hi, i, j, id1, id2;
+	int n = 1;
+	int *answer = NULL;
+	int *array = (int *)malloc(sizeof(int) * numsSize);
+	for (i = 0; i < numsSize; i++)
+	{
+		array[i] = nums[i];
+	}
+	//loÎªnums×î×ó¶Ë£¬hiÎªnums×îÓÒ¶Ë
+	lo = 0;
+	hi = numsSize - 1;
+	sort(array, numsSize);
+	for ( i = 0; i < numsSize; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	while (hi > lo)
+	{
+		if (array[lo] + array[hi] < target)
+			lo++;
+		else if (array[lo] + array[hi] > target)
+			hi--;
+		else
+		{
+			answer = (int *)realloc(answer, 2 * n);
+			for (i = 0; i < numsSize; i++)
+			{
+				if (array[lo] == nums[i])
+				{
+					id1 = i;
+					break;
+				}
 
+			}
+			for (j = numsSize - 1; j >= 0; j--)
+			{
+				if (array[hi] == nums[j])
+				{
+					id2 = j;
+					break;
+				}
+			}
+			answer[2 * n - 2] = id1;
+			answer[2 * n - 1] = id2;
+			n++;
+			hi--;
+		}
+	}
+	return answer;
+}
+
+//Ï£¶ûÅÅÐò
+void sort(int *array, int size)
+{
+	int h = 1;
+	int i, j, temp;
+	while (h < size / 3)
+	{
+		h = 3 * h + 1;
+	}
+	while (h >= 1)
+	{
+		for (i = h; i < size; i++)
+		{
+			for (j = i; j >= h && array[j] <= array[j - h]; j -= h)
+			{
+				temp = array[j];
+				array[j] = array[j - h];
+				array[j - h] = temp;
+			}
+		}
+		h = h / 3;
+	}
 }
